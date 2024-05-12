bae98cce0071d22f16b471d8e5e51a987ea2ee53
Maria Polak <mpolak23@student.aau.dk>
2024-05-12 10:57:26 +0200

    Tilt best I can do

--- Camera/distance.py
+++ Camera/distance.py
@@ -25,12 +25,19 @@ def is_on_the_floor(depth_frame):
         return np.isclose(region, arr_middle, atol=0.05).all()
 
 # Check if the region is tilted (smoothly goes from high to low)
-# If the region is tilted, return the angle of the tilt
+# If the region is tilted and smooth, return the angle of the tilt
 # If the region is not tilted or not smooth, return 0
 def calculate_tilt(depth_frame):
     center_y, center_x = np.array(depth_frame.shape) // 2 # Calculate the center of the depth array
-    area_y, area_x = np.array(depth_frame.shape) // 5 # 20% of depth frame's size
-    region = depth_frame[center_y-area_y:center_y+area_y, center_x-area_x:center_x+area_x] # Extract the 4x4 region around the center
+    circ, _ = np.array(depth_frame.shape) // 5 # 20% of depth frame's size
+    region = depth_frame[center_y-circ:center_y+circ, center_x-circ:center_x+circ] # Extract a region around the center
+    
+    # set all the values besides the corners to the mean of the region
+    corners = [region[0, 0], region[0, -1], region[-1, 0], region[-1, -1]]
+    
+    tilt = (np.max(corners) - np.min(corners)) / 1.5
+    if np.abs(tilt) < config.TILT_THRESHOLD:
+        return 0 
 
     diff_x = np.diff(np.mean(region, axis=1)) # Calculate the difference in depth values along the x-axis
     diff_y = np.diff(np.mean(region, axis=0)) # Calculate the difference in depth values along the y-axis
@@ -38,17 +45,12 @@ def calculate_tilt(depth_frame):
     arr_middle_x = np.full(diff_x.shape, diff_x[0])
     arr_middle_y = np.full(diff_y.shape, diff_y[0])
 
-    if not np.isclose(diff_x, arr_middle_x, atol=0.05).all() or not np.isclose(diff_y, arr_middle_y, atol=0.05).all():
+    if not np.isclose(diff_x, arr_middle_x, atol=0.1).all() or not np.isclose(diff_y, arr_middle_y, atol=0.1).all():
         print("Not tilt")
         return 0
-
-    tilt = (np.max(region) - np.min(region)) / 1.5
-
+    
     ind = np.unravel_index(np.argmin(region, axis=None), region.shape)
     if ind[0] < region.shape[0] // 2:
         tilt = -tilt
-    print(tilt)
-    if np.abs(tilt) < config.TILT_THRESHOLD:
-        return 0 
 
     return np.clip(tilt, -1, 1)
--- Camera/tests/tilt_test.py
+++ Camera/tests/tilt_test.py
@@ -1,11 +1,19 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
 import distance
 import numpy as np
 import matplotlib.pyplot as plt
 
 tilt = np.linspace(-1, 1, 100)
 
-x = np.linspace(0, 0.2, 1000)
-y = np.linspace(0, 0.9, 1000)
+x = np.linspace(0, 1, 100)
+y = np.linspace(0, 1, 100)
 
 xx, yy = np.meshgrid(x, y)
 not_tilt = np.sqrt(xx**2 + yy**2)
@@ -22,10 +30,29 @@ tilt_3 = (xx - yy)
 xx, yy = np.meshgrid(x, y)
 tilt_4 = -(xx - yy)
 
-print("Not tilt", distance.calculate_tilt(not_tilt))
-print("Tilt", distance.calculate_tilt(tilt))
-print("Tilt 2", distance.calculate_tilt(tilt_2))
-print("Tilt 3", distance.calculate_tilt(tilt_3))
-print("Tilt 4", distance.calculate_tilt(tilt_4))
+print("Not tilt:", distance.calculate_tilt(not_tilt))
+print("Tilt:", distance.calculate_tilt(tilt))
+print("Tilt 2:", distance.calculate_tilt(tilt_2))
+print("Tilt 3:", distance.calculate_tilt(tilt_3))
+print("Tilt 4:", distance.calculate_tilt(tilt_4))
 
 
+# Add noise
+noise = np.random.normal(0, 0.09, (100, 100)) # 0.5 is the threshold where we still get tilt
+
+tilt = tilt + noise
+tilt_2 = tilt_2 + noise
+tilt_3 = tilt_3 + noise
+tilt_4 = tilt_4 + noise
+print("Tilt + Noise:", distance.calculate_tilt(tilt))
+print("Tilt 2 + Noise:", distance.calculate_tilt(tilt_2))
+print("Tilt 3 + Noise:", distance.calculate_tilt(tilt_3))
+print("Tilt 4 + Noise:", distance.calculate_tilt(tilt_4))
+
+# plt.imshow(tilt)
+# plt.show()
+
+# read image from aaaa30.png
+depth_frame = plt.imread("../images/aaaa30.png")
+
+print("Image", distance.calculate_tilt(np.rot90(depth_frame * 3, k=1)))
